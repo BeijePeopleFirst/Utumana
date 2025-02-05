0cd6a8e2ab7de604e44c4d02b2d25832c89bd90c
anita.papetti <anita.papetti@gmail.com>
2025-02-05 15:21:45 +0100

    + login and logout in frontend

--- angular_frontend/src/app/components/login/login.component.ts
+++ angular_frontend/src/app/components/login/login.component.ts
@@ -12,7 +12,7 @@ import { AuthService } from 'src/app/services/auth.service';
 export class LoginComponent implements OnInit {
   emailError: boolean = false;  // true if backend has sent error: email not found
   passwordError: boolean = false; // true if backend has sent error: wrong password
-  error500: boolean = false;  // true if backend has sent error with staus 500
+  genericError: boolean = false;  // true if backend has sent error with staus 500
   showEmailError: boolean = false;
   showPasswordError: boolean = false;
   user: AuthCredentials = {email: '', password: ''};
@@ -40,5 +40,24 @@ export class LoginComponent implements OnInit {
       this.showPasswordError = true;
       return;
     }
+    this.authService.login(this.user).subscribe(res => {
+      console.log("AuthService: login response", res);
+      if(res.ok === true){
+        this.router.navigateByUrl(this.returnUrl);
+      }else{
+        if(res.status == 401){
+          if(res.message.includes("password")){
+            this.passwordError = true;
+            this.showPasswordError = true;
+          }else{
+            this.emailError = true;
+            this.showEmailError = true;
+            console.log("email error");
+          }
+        }else{
+            this.genericError = true;
+        }
+      }
+    })
   }
 }
--- angular_frontend/src/app/services/auth.service.ts
+++ angular_frontend/src/app/services/auth.service.ts
@@ -1,26 +1,65 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
-import { tap, delay } from 'rxjs/operators';
+import { tap, delay, map, catchError } from 'rxjs/operators';
+import { BACKEND_URL_PREFIX } from 'src/costants';
+import { AuthCredentials } from '../dtos/authCredential';
+
+interface LoginResponse {
+  email: string,
+  permission: string,
+  token: string,
+  refresh_token: string,
+  id: number
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   isLoggedIn = false;
-
-  // store the URL so we can redirect after logging in
   redirectUrl: string | null = null;
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json; charset=UTF-8', 'Accept-Type': 'application/json; charset=UTF-8' })
+  };
 
-  login(): Observable<boolean> {
-    // TODO
-    return of(true).pipe(
-      delay(1000),
-      tap(() => this.isLoggedIn = true)
+  constructor(
+    private router: Router,
+    private http: HttpClient
+  ){ }
+
+  login(user: AuthCredentials): Observable<{ok: boolean, status: number, message: string}> {
+    return this.http.post<LoginResponse>(`${BACKEND_URL_PREFIX}/api/signin`, user, this.httpOptions).pipe(
+      map(json => {
+        console.log(json);
+        localStorage.setItem('id',json.id.toString());
+			  localStorage.setItem("token", json.token);
+			  document.cookie = "refresh_token=" + json.refresh_token;
+        return {ok: true, status: 200, message: 'Successfully logged in'};
+      }),
+      tap(() => this.isLoggedIn = true),
+      catchError((err) => {
+        console.log(err);
+        return of({ok: false, status: Number(err.error.status), message: err.error.message});
+      })
     );
   }
 
   logout(): void {
-    // TODO
     this.isLoggedIn = false;
+    localStorage.clear();
+    sessionStorage.clear();
+    this.deleteCookies();
+    this.router.navigate(['login']);
   }
+
+  private deleteCookies(): void {
+    let allCookies = document.cookie.split(';');
+    
+    // The "expire" attribute of every cookie is 
+    // Set to "Thu, 01 Jan 1970 00:00:00 GMT"
+    for (let i = 0; i < allCookies.length; i++)
+      document.cookie = allCookies[i] + "=;expires=" + new Date(0).toUTCString();
+    }
 }
