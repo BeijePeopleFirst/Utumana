ca89c71e7eb1f53654f615ff1606e8737faa43f1
Riccardo_Gugolati <gugolati.riccardo@gmail.com>
2025-02-21 18:19:43 +0100

    BookNow from homepage work in progress

--- angular_frontend/src/app/components/book-now/book-now.component.ts
+++ angular_frontend/src/app/components/book-now/book-now.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { USE_DEFAULT_LANG } from '@ngx-translate/core';
 import { Accommodation } from 'src/app/models/accommodation';
 import { Availability } from 'src/app/models/availability';
 import { AccommodationService } from 'src/app/services/accommodation.service';
@@ -17,11 +18,19 @@ export class BookNowComponent implements OnInit {
   selectedAccommodationAvailabilities: Availability[] = [];
   //------------------------------------------------------------------------------------------------
 
+  //Chosen dates:
+  //------------------------------------------------------------------------------------------------
+  selectedCheckIn?: number;
+  selectedCheckoout?: number;
+  //------------------------------------------------------------------------------------------------
+
   //Messagges:
   //------------------------------------------------------------------------------------------------
   thereAreMessagesToDiplay: boolean = false;
   invalidAccommodationError: boolean = false;
   noAccommodationIdWasProvidedError: boolean = false;
+  errorOccurredWhileFetchingMonthNameOrIndexError: boolean = false;
+  invalidChosenDayError: boolean = false;
   //------------------------------------------------------------------------------------------------
 
   // Data corrente per il calendario principale
@@ -65,13 +74,34 @@ export class BookNowComponent implements OnInit {
   }
 
   // Getter per il nome del mese corrente (es. "marzo")
-  get currentMonth(): string {
-    return this.currentDate.toLocaleString('default', { month: 'long' });
+  public getMonthName(monthIndexLocal?: number): string {
+    let monthIndex: number = monthIndexLocal ? monthIndexLocal : this.currentDate.getMonth();
+
+    switch(monthIndex) {
+      case 0: return "january";
+      case 1: return "february";
+      case 2: return "march";
+      case 3: return "april";
+      case 4: return "may";
+      case 5: return "june";
+      case 6: return "july";
+      case 7: return "august";
+      case 8: return "september";
+      case 9: return "october";
+      case 10: return "november";
+      case 11: return "december";
+      default: return "Error";
+    }
   }
 
   // Getter per l'anno corrente
   get currentYear(): number {
     return this.currentDate.getFullYear();
+  }
+
+  get nextMonthYear(): number {
+    if(this.currentDate.getMonth() === 11) return this.currentYear + 1;
+    else return this.currentYear;
   }
 
   // Passa al mese precedente e rigenera i calendari
@@ -126,8 +156,103 @@ export class BookNowComponent implements OnInit {
 
 // Metodo per ottenere il nome del mese successivo
 public getNextMonthName(): string {
+  
+  if(this.currentDate.getMonth() === 11) return "january";
+
   const nextMonthDate = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() + 1, 1);
-  return nextMonthDate.toLocaleString('default', { month: 'long' });
+  console.log(this.getMonthName(nextMonthDate.getMonth()));
+  return this.getMonthName(nextMonthDate.getMonth());
+}
+
+resetSelectedDates() {
+  this.selectedCheckIn = undefined;
+  this.selectedCheckoout = undefined;
+}
+
+isSelected(year: number, monthName: string, day: number | null): boolean {
+
+  if(!day) return false;
+  let toTest: number = Date.parse(new Date(year, this.getMonthNumberByName(monthName), day) + "");
+
+  return this.checkIfIsBetween(toTest, this.selectedCheckIn!, this.selectedCheckoout!);
+}
+
+nextMonthName(): string {
+  if(this.getMonthName() === "december") return "january";
+  else {
+    let number: number = this.currentDate.getMonth() + 1;
+    return this.getMonthName(number);
+  }
+}
+
+selectDay(y: number, monthName: string, day: number | null): void {
+  if(!day) {
+    this.invalidChosenDayError = true;
+    this.thereAreMessagesToDiplay = true;
+    return;
+  }
+  let chosenDate: number = Date.parse(new Date(y, this.getMonthNumberByName(monthName), day) + "");
+
+  if(!this.selectedCheckIn) this.selectedCheckIn = chosenDate;
+  else {
+    this.selectedCheckoout = chosenDate;
+    console.log(new Date(this.selectedCheckIn!), new Date(this.selectedCheckoout!));
+  }
+}
+
+//TODO:
+//Need to keep in mind both "real" booking and unavailability option
+//After the storing procedure you should send a message into the GUI AND then redirect to the Homepage
+confirmSelectedDates(): void {
+
+}
+
+isAnAvailability(y: number, monthName: string, day: number | null): boolean {
+  let monthNumber: number = this.getMonthNumberByName(monthName);
+
+  if(!day) return false;
+
+  if(monthNumber === -1) {
+    this.thereAreMessagesToDiplay = true;
+    this.errorOccurredWhileFetchingMonthNameOrIndexError = true;
+    return false;
+  }
+
+  let dateToAnalize: number = Date.parse(new Date(y, monthNumber, day) + "");
+
+  let start: number;
+  let end: number;
+  for(let a of this.accommodation.availabilities) {
+    start = Date.parse(a.start_date);
+    end = Date.parse(a.end_date);
+
+    if(this.checkIfIsBetween(dateToAnalize, start, end)) return true;
+  }
+
+  return false;
+}
+
+private checkIfIsBetween(test: number, start: number, end: number): boolean {
+  if(test >= start && test <= end) return true;
+  return false;
+}
+
+private getMonthNumberByName(name: string): number {
+  switch(name) {
+    case "january": return 0;
+    case "february": return 1;
+    case "march": return 2;
+    case "april": return 3;
+    case "may": return 4;
+    case "june": return 5;
+    case "july": return 6;
+    case "august": return 7;
+    case "september": return 8;
+    case "october": return 9;
+    case "november": return 10;
+    case "december": return 11;
+    default: return -1;
+  }
 }
 
 public clearMessages(): void {
@@ -135,6 +260,8 @@ public clearMessages(): void {
 
   this.invalidAccommodationError = false;
   this.noAccommodationIdWasProvidedError = false;
+  this.errorOccurredWhileFetchingMonthNameOrIndexError = false;
+  this.invalidChosenDayError = false;
 }
 
 }
