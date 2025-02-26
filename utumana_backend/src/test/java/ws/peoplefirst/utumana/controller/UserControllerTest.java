c9f05b3b228f1045fb0a75d5f3b4539223bfad92
RaffaeleSaponara <raffaele.saponara@beije.it>
2025-02-26 10:03:00 +0100

    userControllerTest

--- utumana_backend/src/test/java/ws/peoplefirst/utumana/controller/UserControllerTest.java
+++ utumana_backend/src/test/java/ws/peoplefirst/utumana/controller/UserControllerTest.java
@@ -1,13 +1,31 @@
 package ws.peoplefirst.utumana.controller;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.Authentication;
 import ws.peoplefirst.utumana.dto.UserDTO;
+import ws.peoplefirst.utumana.exception.DBException;
+import ws.peoplefirst.utumana.exception.IdNotFoundException;
+import ws.peoplefirst.utumana.model.BadgeAward;
+import ws.peoplefirst.utumana.model.User;
 import ws.peoplefirst.utumana.service.UserService;
+import ws.peoplefirst.utumana.utility.AuthorizationUtility;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -23,6 +41,8 @@ public class UserControllerTest {
 
     @Mock
     private UserDTO userDTO;
+
+    private MockedStatic<AuthorizationUtility> authorizationUtilityMock;
 
     /**
      * Reusable methods
@@ -40,17 +60,134 @@ public class UserControllerTest {
         return new UserDTO(1L, "", "", "");
     }
 
+    @BeforeEach
+    void setUp() {
+        authorizationUtilityMock = Mockito.mockStatic(AuthorizationUtility.class);
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (authorizationUtilityMock != null) {
+            authorizationUtilityMock.close();
+        }
+    }
+
+    /**
+     * GET - API: /users
+     * MTEHOD: getUser(...)
+     */
+
+    @Test
+    void getUsersDTO(){
+        assertDoesNotThrow(() -> userController.getUsersDTO());
+    }
+
+    /**
+     * GET - API: /users_full_obj
+     * MTEHOD: getAllUsersFullObj(...)
+     */
+
+    @Test
+    void getUsersgetAllUsersFullObjDTO(){
+        assertDoesNotThrow(() -> userController.getAllUsersFullObj(authentication));
+    }
+
     /**
      * GET - API: /user/{id}
      * MTEHOD: getUser(...)
      */
 
     @Test
-    void getUserKO(){
+    void getUserKo(){
+//        authorizationUtilityMock = mockStatic(AuthorizationUtility.class);
+        authorizationUtilityMock.when(() -> AuthorizationUtility.checkIsAdminOrMe(authentication, 1L))
+                .thenAnswer(invocation -> null);
+        when(userService.getUserForProfile(1L)).thenThrow(new IdNotFoundException("User not found"));
+        assertThrows(IdNotFoundException.class, () -> userController.getUser(1L, authentication));
+    }
+
+    /**
+     * POST - API: /user
+     * MTEHOD: insertUser(...)
+     */
+
+    @Test
+    void insertUser(){
+        assertDoesNotThrow(() -> userController.insertUser(getInstance(User.class)));
+    }
+
+    /**
+     * POST - API: /change_password
+     * MTEHOD: modifyPassword(...)
+     */
+
+    @Test
+    void modifyPasswordSuccess(){
+        when(userService.getUserByEmail(anyString())).thenReturn(getInstance(User.class));
+        when(userService.isValidPassword(anyString())).thenReturn(true);
+        when(userService.saveUser(any(User.class))).thenReturn(true);
+        assertDoesNotThrow(() -> userController.modifyPassword(anyMap()));
     }
 
     @Test
-    void getUser(){
+    void modifyPasswordKoDbSave(){
+        when(userService.getUserByEmail(anyString())).thenReturn(getInstance(User.class));
+        when(userService.isValidPassword(anyString())).thenReturn(true);
+        when(userService.saveUser(any(User.class))).thenReturn(false);
+        assertThrows(DBException.class, () -> userController.modifyPassword(anyMap()));
+    }
+
+    @Test
+    void modifyPasswordKoIdNotFound(){
+        when(userService.getUserByEmail(anyString())).thenReturn(null);
+        assertThrows(IdNotFoundException.class, () -> userController.modifyPassword(anyMap()));
+    }
+
+    /**
+     * POST - API: /user/badges/{userId}
+     * MTEHOD: getAllUserBadges(...)
+     */
+
+    @Test
+     void getAllUserBadges(){
+        when(userService.getAllUserBadges(anyLong())).thenReturn(List.of(getInstance(BadgeAward.class)));
+        assertDoesNotThrow(() -> userController.getAllUserBadges(anyLong()));
+    }
+
+    /**
+     * PATCH - API: /user}
+     * MTEHOD: modifyUser(...)
+     */
+
+    @Test
+    void modifyUserSuccess(){
+        Map<String , Object> mockMap = new HashMap<>();
+        mockMap.put("id", 1L);
+        mockMap.put("name", "pollo");
+        mockMap.put("surname", "alberto");
+        mockMap.put("email", "alberto@pollo.it");
+        mockMap.put("password", "1234");
+        mockMap.put("bio", "hello my name is Pollo");
+        mockMap.put("profile_picture", "questa è un immagine");
+        mockMap.put("archived_timestamp", "boh");
+
+
+//        authorizationUtilityMock = mockStatic(AuthorizationUtility.class);
+
+        authorizationUtilityMock.when(() -> AuthorizationUtility.checkIsAdminOrMe(authentication, 1L))
+                .thenAnswer(invocation -> null);
+        authorizationUtilityMock.when(() -> AuthorizationUtility.hasAdminRole(authentication))
+                .thenAnswer(invocation -> true);
+        when(userService.isValidName(anyString())).thenReturn(true);
+        when(userService.isValidSurname(anyString())).thenReturn(true);
+        when(userService.isValidEmail(anyString())).thenReturn(true);
+        when(userService.isEmailUnique(anyString(), anyLong())).thenReturn(true);
+        when(userService.isValidPassword(anyString())).thenReturn(true);
+        when(userService.getUserById(anyLong())).thenReturn(getInstance(User.class));
+
+
+        assertDoesNotThrow(() -> userController.modifyUser(mockMap, authentication));
+       // assertThrows(ForbiddenException.class, () -> userController.modifyUser(mockMap, authentication));
     }
 
 }
