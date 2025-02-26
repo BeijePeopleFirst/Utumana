1f24172ccc5c9546a07ebb9891b3ceb71002a661
AndreaBiasion02 <a.biasionsomaschini@gmail.com>
2025-02-26 16:03:06 +0100

    add lat and lon in properties

--- utumana_backend/src/main/java/ws/peoplefirst/utumana/AddressConfiguration.java
+++ utumana_backend/src/main/java/ws/peoplefirst/utumana/AddressConfiguration.java
@@ -23,6 +23,8 @@ public class AddressConfiguration {
     public static class DefaultAddress {
         private String name;
         private String address;
+        private Double lat;
+        private Double lon;
 
         public String getName() {
             return name;
@@ -35,6 +37,20 @@ public class AddressConfiguration {
         }
         public void setAddress(String address) {
             this.address = address;
+        }
+
+        public Double getLat() {
+            return lat;
+        }
+        public void setLat(Double lat) {
+            this.lat = lat;
+        }
+
+        public Double getLon() {
+            return lon;
+        }
+        public void setLon(Double lon) {
+            this.lon = lon;
         }
         
         
