1a17724902f3013eb7bf7413cb55961330c59e19
Riccardo Gugolati <gugolati.riccardo@gmail.com>
2025-03-24 12:25:19 +0100

    INIT home panel admin dashboard

--- angular_frontend/src/app/components/admin-dashboard-home-panel-component/admin-dashboard-home-panel-component.component.ts
+++ angular_frontend/src/app/components/admin-dashboard-home-panel-component/admin-dashboard-home-panel-component.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccommodationService } from 'src/app/services/accommodation.service';
 import { imagesURL } from 'src/costants';
 
 @Component({
@@ -6,6 +8,32 @@ import { imagesURL } from 'src/costants';
   templateUrl: './admin-dashboard-home-panel-component.component.html',
   styleUrls: ['./admin-dashboard-home-panel-component.component.css']
 })
-export class AdminDashboardHomePanelComponent {
-  url = imagesURL + "\\house_manager.jpg";
+export class AdminDashboardHomePanelComponent implements OnInit {
+
+  public totalAccommodations!: number;
+  public displaySpinner: boolean = true;
+  
+  constructor(
+    private accommodationService: AccommodationService,
+    private router: Router
+  )
+  {}
+
+  ngOnInit(): void {
+    this.loadPendingAccommodations();
+  }
+
+  loadPendingAccommodations(): void {
+      this.accommodationService.getAccommodationsToBeApproved().subscribe(accommodations => {
+        this.accommodationService.getPrices(accommodations).subscribe(updated => {
+          this.totalAccommodations = updated.length;
+          this.displaySpinner = false
+        });
+      });
+    }
+
+  goToAcceptOrRejectAccommodations(): void {
+    this.router.navigate(["/admin-dashboard/accommodations/accept-reject"]);
+  }
+
 }
